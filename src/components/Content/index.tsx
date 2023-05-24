850c238d17a7cbc09592877d3cd90b78fa4c35d2
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2023-05-24 11:28:31 -0300

    Criando camada de serviço

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,116 +1,130 @@
-import styles from './index.module.css';
-import Plus from '../../assets/plus.svg';
-import { NoContent } from '../NoContent';
-import { ChangeEvent, useEffect, useState } from 'react';
-import { TodoList } from '../TodoList';
-import { Task } from '../../models/Task';
-import { v4 as uuidv4 } from 'uuid';
-import { api } from '../../configs/api';
-import useToDoContext from '../../hooks/useToDoContext';
-import { useToast } from '../../hooks/useToast';
+import { ChangeEvent, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Plus from "../../assets/plus.svg";
+import useToDoContext from "../../hooks/useToDoContext";
+import { useToast } from "../../hooks/useToast";
+import {
+  changeIsDone,
+  createTask,
+  deleteTask,
+  getTasks,
+} from "../../services/task";
+import { NoContent } from "../NoContent";
+import { TodoList } from "../TodoList";
+import styles from "./index.module.css";
 
 export const Content = () => {
-    const [description, setDescription] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
 
-    const { taskListState, setTaskListState } = useToDoContext()
-    const { showToast } = useToast();
+  const { taskListState, setTaskListState } = useToDoContext();
+  const { showToast } = useToast();
 
-    const tasksDone = taskListState.filter((task) => {
-        return task.isDone !== false;
-    })
+  const tasksDone = taskListState.filter((task) => {
+    return task.isDone !== false;
+  });
 
-    const disabledButton = !description.length;
+  const disabledButton = !description.length;
 
-    const addTaskOnList = () => {
+  const addTaskOnList = () => {
+    const newTask = {
+      id: uuidv4(),
+      description,
+      isDone: false,
+    };
 
-        const newTask = {
-            id: uuidv4(),
-            description,
-            isDone: false,
-        }
-        api.post("tasks", newTask)
-            .then((response) => setTaskListState((currentValue) => [...currentValue, response.data]))
-            .finally(() => {
-                setDescription('')
-                showToast({
-                    message: "Tarefa adicionada com sucesso",
-                    type: 'success'
-                })
-            });
-    }
-
-    const removeTaskOnList = (id: string) => {
-        api.delete(`tasks/${id}`)
-            .then(() =>
-                setTaskListState((currentValue) => currentValue.filter(task => task.id !== id)));
-    }
-
-    const changeStatusCheckbox = (id: string) => {
-        const task = taskListState.find(task => task.id === id);
-
-        if (task) {
-            api.patch(`tasks/${id}`, {
-                isDone: !task.isDone,
-            });
-        }
-
-        const xebas = taskListState.map((task) => {
-            if (task.id === id) {
-                return {
-                    ...task,
-                    isDone: !task.isDone
-                }
-            }
-            return task;
+    createTask(newTask)
+      .then((response) =>
+        setTaskListState((currentValue) => [...currentValue, response])
+      )
+      .finally(() => {
+        setDescription("");
+        showToast({
+          message: "Tarefa adicionada com sucesso",
+          type: "success",
         });
+      });
+  };
 
-        setTaskListState(xebas);
+  const removeTaskOnList = (id: string) => {
+    deleteTask(id).then(() =>
+      setTaskListState((currentValue) =>
+        currentValue.filter((task) => task.id !== id)
+      )
+    );
+  };
+
+  const changeStatusCheckbox = (id: string) => {
+    const task = taskListState.find((task) => task.id === id);
+
+    if (task) {
+      changeIsDone(id, {
+        isDone: !task.isDone,
+      });
     }
 
-    useEffect(() => {
-        api.get("tasks").then((response) => setTaskListState(response.data as Task[]));
-    }, []);
+    const xebas = taskListState.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          isDone: !task.isDone,
+        };
+      }
+      return task;
+    });
 
-    return (
-        <section className={styles.section_container}>
-            <main>
+    setTaskListState(xebas);
+  };
 
-                <article className={styles.input_container}>
-                    <input
-                        className={styles.input}
-                        type="text"
-                        value={description}
-                        placeholder="Adicione uma nova tarefa"
-                        onChange={(event: ChangeEvent<HTMLInputElement>) => setDescription(event.target.value)} />
-                    <button
-                        className={styles.button}
-                        disabled={disabledButton}
-                        onClick={() => addTaskOnList()} >
-                        Criar
-                        <img
-                            src={Plus}
-                            alt="Ícone de mais" />
-                    </button>
-                </article>
+  useEffect(() => {
+    getTasks().then((response) => setTaskListState(response));
+  }, [setTaskListState]);
 
-                <article className={styles.content_header}>
-                    <article className={styles.tasks_container}>
-                        <p className={styles.tasks_created}>Tarefas Criadas</p>
-                        <span className={styles.span_value}>{taskListState.length}</span>
-                    </article>
-                    <article className={styles.tasks_container}>
-                        <p className={styles.tasks_done}>Concluídas</p>
-                        <span className={styles.span_value}> {tasksDone.length} de {taskListState.length} </span>
-                    </article>
-                </article>
+  return (
+    <section className={styles.section_container}>
+      <main>
+        <article className={styles.input_container}>
+          <input
+            className={styles.input}
+            type="text"
+            value={description}
+            placeholder="Adicione uma nova tarefa"
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              setDescription(event.target.value)
+            }
+          />
+          <button
+            className={styles.button}
+            disabled={disabledButton}
+            onClick={() => addTaskOnList()}
+          >
+            Criar
+            <img src={Plus} alt="Ícone de mais" />
+          </button>
+        </article>
 
-                {taskListState.length === 0 ? <NoContent /> : <TodoList
-                    onDelete={removeTaskOnList}
-                    onChangeCheckbox={changeStatusCheckbox}
-                />}
+        <article className={styles.content_header}>
+          <article className={styles.tasks_container}>
+            <p className={styles.tasks_created}>Tarefas Criadas</p>
+            <span className={styles.span_value}>{taskListState.length}</span>
+          </article>
+          <article className={styles.tasks_container}>
+            <p className={styles.tasks_done}>Concluídas</p>
+            <span className={styles.span_value}>
+              {" "}
+              {tasksDone.length} de {taskListState.length}{" "}
+            </span>
+          </article>
+        </article>
 
-
-            </main>
-        </section>
-    )
-}
+        {taskListState.length === 0 ? (
+          <NoContent />
+        ) : (
+          <TodoList
+            onDelete={removeTaskOnList}
+            onChangeCheckbox={changeStatusCheckbox}
+          />
+        )}
+      </main>
+    </section>
+  );
+};
--- src/configs/api/index.ts
+++ src/configs/api/index.ts
@@ -1,5 +1,16 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333/'
+  baseURL: "http://localhost:3333",
+  timeout: 5000,
 });
+
+export const apiRequest = async <T>(config: AxiosRequestConfig): Promise<T> => {
+  try {
+    const response = await api(config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
