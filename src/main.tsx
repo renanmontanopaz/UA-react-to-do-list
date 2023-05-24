1f6f9f9c270c0fd34effee2361caf3e0d5c651eb
Castegnaro <matheuscastegnaro@gmail.com>
2023-05-23 21:09:18 -0300

    Criando estrutura de roteamento

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './global.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import NotFound from './components/NotFound/index.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
