fb632f2c94f84075f9037486bab05e7d2cc88ab4
Matheus Castegnaro <matheuscastegnaro@hotmail.com>
2023-05-27 13:15:28 -0300

    Configurando lazy

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,40 @@
-import React from "react";
+/* eslint-disable react-refresh/only-export-components */
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./global.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NotFound from "./components/NotFound/index.tsx";
-import LandingPage from "./pages/LandingPage/index.tsx";
-import ToDo from "./pages/ToDo/index.tsx";
+
+const LandingPage = lazy(() => import("./pages/LandingPage"));
+const ToDo = lazy(() => import("./pages/ToDo"));
+const NotFound = lazy(() => import("./components/NotFound"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage />,
-    errorElement: <NotFound />,
+    element: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <LandingPage />
+      </Suspense>
+    ),
+    errorElement: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <NotFound />
+      </Suspense>
+    ),
   },
   {
     path: "/to-do",
-    element: <ToDo />,
-    errorElement: <NotFound />,
+    element: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <ToDo />
+      </Suspense>
+    ),
+    errorElement: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <NotFound />
+      </Suspense>
+    ),
   },
 ]);
 
