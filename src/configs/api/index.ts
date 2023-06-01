f1df24311c23b9c4266b1e0b29f4174b587365c2
Castegnaro <matheuscastegnaro@gmail.com>
2023-06-01 20:36:19 -0300

    Ajustando URL

--- src/configs/api/index.ts
+++ src/configs/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://api.jsonserver.io/tasks',
+    baseURL: 'https://api.jsonserver.io/',
     headers: {
         "X-Jsio-Token": "69008bbc053a751d077a2d69d13255ca"
     }
