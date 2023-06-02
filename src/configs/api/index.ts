acb192709266d0ec57d216bb8367724c1f5765f5
Castegnaro <matheuscastegnaro@gmail.com>
2023-06-01 21:24:22 -0300

    Ajustando json-server

--- src/configs/api/index.ts
+++ src/configs/api/index.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://api.jsonserver.io/',
-    headers: {
-        "X-Jsio-Token": "69008bbc053a751d077a2d69d13255ca"
-    }
+    baseURL: 'https://ua-todo-list-api.onrender.com/',
 });
