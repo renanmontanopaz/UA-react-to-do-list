c9d58610c94f26d7074d3823483c5dc1b029c995
Castegnaro <matheuscastegnaro@gmail.com>
2023-06-15 20:07:40 -0300

    Chore: criando arquivo de test

---
+++ src/pages/LandingPage/__test__/LandingPage.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('<LandingPage>', () => {
+  it('Deve renderizar a landing page corretamente', () => {
+    expect(true).toBe(true);
+  })
+}) 
